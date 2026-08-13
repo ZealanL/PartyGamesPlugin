af960656438e23ae3d45d41592bdcabe2f1a29e4
ZealanL <36944229+ZealanL@users.noreply.github.com>
2026-08-13 05:39:17 -0700

    Remove stats from gitignore

---
+++ src/main/java/zealan/pgp/stats/PlayerStatsMgr.java
@@ -0,0 +1,169 @@
+package zealan.pgp.stats;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import zealan.pgp.AutoListener;
+import zealan.pgp.api.command.CommandArg;
+import zealan.pgp.api.command.CommandNode;
+import zealan.pgp.api.command.CommandResult;
+import zealan.pgp.game.Game;
+import zealan.pgp.game.Gamer;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+import static zealan.pgp.Globals.*;
+
+public class PlayerStatsMgr extends AutoListener {
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().enableComplexMapKeySerialization().create();
+
+    private static final int STATS_FILE_VERSION = 2;
+    private static final Path JSON_PATH = PLUGIN.getDataFolder().toPath().resolve("stats_v" + STATS_FILE_VERSION + ".json");
+
+    private static final int SAVE_INTERVAL_TICKS = 120 * 20;
+    private int ticksSinceSave = 0;
+    private final HashMap<UUID, PlayerStatsRecord> records = new HashMap<>();
+
+    public PlayerStatsMgr() {
+        COMMAND_SYS.register(CommandNode.make("stats", "Display a player's stats", (ctx) -> {
+            String playerName = ctx.getArg("player_name");
+            if (playerName == null) playerName = ctx.sender.getName();
+
+            var stats = getPlayerStatsByName(playerName);
+            if (stats == null) return CommandResult.failure("No stats are recorded for \"" + playerName + "\"");
+
+            // TODO: Book implementation
+                    /*
+                    var book = new org.bukkit.inventory.ItemStack(org.bukkit.Material.WRITTEN_BOOK);
+                    var bookMeta = (org.bukkit.inventory.meta.BookMeta) book.getItemMeta();
+
+                    bookMeta.setTitle("Player Stats");
+                    bookMeta.setAuthor("[SERVER]");
+                    bookMeta.addPages(
+                            stats.toPrintOutText().toAdventureComponent()
+                    );
+
+                    book.setItemMeta(bookMeta);
+                    ctx.sender.getBukkitEntity().openBook(book);
+                    */
+
+            return CommandResult.ok(stats.toPrintOutMsg());
+
+        }, new CommandArg.AnyString("player_name").makeOptional()));
+        COMMAND_SYS.register(CommandNode.make("reloadstats", "Reload all player stats", ctx -> {
+            this.load();
+            // TODO: ADD
+            //LEADERBOARD_MGR.fullUpdate();
+            return CommandResult.ok("Reloaded!");
+        }).withOpOnly());
+
+        load();
+    }
+
+    private void load() {
+        if (!Files.exists(JSON_PATH)) {
+            save();
+            return;
+        }
+
+        try (FileReader reader = new FileReader(JSON_PATH.toFile())) {
+            Type type = new TypeToken<HashMap<UUID, PlayerStatsRecord>>() {
+            }.getType();
+            HashMap<UUID, PlayerStatsRecord> loadedRecords = GSON.fromJson(reader, type);
+
+            records.clear();
+            if (loadedRecords != null) records.putAll(loadedRecords);
+
+            PLOG.info("PlayerStatsManager(): Loaded stats for " + records.size() + " players");
+        } catch (IOException e) {
+            PLOG.severe("PlayerStatsManager(): FAILED to load player stats: " + e);
+        }
+    }
+
+    private void save() {
+        ticksSinceSave = 0;
+
+        try {
+            Files.createDirectories(JSON_PATH.getParent());
+            try (FileWriter writer = new FileWriter(JSON_PATH.toFile())) {
+                GSON.toJson(records, writer);
+            }
+            PLOG.info("PlayerStatsManager(): Saved stats for " + records.size() + " players");
+        } catch (IOException e) {
+            PLOG.severe("PlayerStatsManager(): FAILED to save all player stats: " + e);
+        }
+    }
+
+    public Collection<PlayerStatsRecord> getAllRecords() {
+        return records.values();
+    }
+
+    public PlayerStatsRecord getPlayerStats(Player player) {
+        return getPlayerStatsByUUID(player.getUniqueId());
+    }
+
+    public PlayerStatsRecord getPlayerStatsByUUID(UUID uuid) {
+        return records.get(uuid);
+    }
+
+    public PlayerStatsRecord getPlayerStatsByName(String playerName) {
+        for (var entry : records.entrySet())
+            if (entry.getValue().playerName.equalsIgnoreCase(playerName)) return entry.getValue();
+
+        return null;
+    }
+
+    public void onPlayerStoppedPlaying(Gamer gp, Game game, boolean finished) {
+        if (gp.isPlaying()) throw new RuntimeException("Player has not finished");
+
+        var record = getPlayerStats(gp.player);
+        record.updateAfterPlaying(gp, game, finished);
+
+        // TODO: ADD
+        //Bukkit.getScheduler().runTaskLater(PLUGIN, () -> {
+        //    LEADERBOARD_MGR.updateForPlayer(gp.ent, game.getConfig(), true);
+        //}, 2 * 20);
+    }
+
+    // ///////////
+
+    @EventHandler
+    protected void onPlayerJoin(PlayerJoinEvent event) {
+        var player = event.getPlayer();
+        var record = getPlayerStats(player);
+        if (record == null) {
+            record = new PlayerStatsRecord(player);
+            records.put(player.getUniqueId(), record);
+        }
+
+        record.numJoins++;
+    }
+
+    @EventHandler
+    protected void onPlayerLeave(PlayerQuitEvent event) {
+        var player = event.getPlayer();
+        var record = getPlayerStats(player);
+        if (record == null) throw new RuntimeException("Player left with no stats accessible!?");
+
+        record.numLeaves++;
+    }
+
+    @Override
+    public void onTick() {
+        ticksSinceSave++;
+        if (ticksSinceSave >= SAVE_INTERVAL_TICKS) save();
+    }
+}
---
+++ src/main/java/zealan/pgp/stats/PlayerStatsRecord.java
@@ -0,0 +1,256 @@
+package zealan.pgp.stats;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import zealan.pgp.api.display.Display;
+import zealan.pgp.game.Game;
+import zealan.pgp.game.GameConfig;
+import zealan.pgp.game.GameVariant;
+import zealan.pgp.game.Gamer;
+
+import java.time.Instant;
+import java.util.*;
+
+import static zealan.pgp.Globals.PLOG;
+
+public class PlayerStatsRecord {
+    public final UUID playerUUID;
+    public String playerName;
+
+    public int numJoins = 0;
+    public int numLeaves = 0;
+
+    public static class GamePB {
+        public enum Type {
+            POINTS,
+            FASTEST_TIME,
+            LONGEST_TIME,
+        }
+
+        public final Type type;
+        public final int val; // Number of points OR ticks played for
+        public final long time;
+
+        GamePB(GameConfig gameCfg, Gamer player) {
+            this.type = switch (gameCfg.style) {
+                case POINTS -> Type.POINTS;
+                case SURVIVAL -> Type.LONGEST_TIME;
+                case RACE -> Type.FASTEST_TIME;
+            };
+            this.val = switch (gameCfg.style) {
+                case POINTS -> player.getScore();
+                case SURVIVAL, RACE -> player.getTicksPlayedFor();
+            };
+
+            this.time = Instant.now().getEpochSecond();
+        }
+
+        public boolean isBetterThan(GamePB other) {
+            if (this.type != other.type)
+                throw new IllegalArgumentException("PB types are different");
+
+            return switch (this.type) {
+                case POINTS, LONGEST_TIME -> this.val > other.val;
+                case FASTEST_TIME -> this.val < other.val;
+            };
+        }
+
+        public String toMsgPart() {
+            Object valDisplay = switch (this.type) {
+                case POINTS -> String.valueOf(this.val);
+                case FASTEST_TIME, LONGEST_TIME -> new Display.TicksTime(this.val, true);
+            };
+
+            return Display.format("{}", valDisplay);
+        }
+
+        public static int compare(GamePB a, GamePB b) {
+            if (a.isBetterThan(b)) return -1;
+            if (b.isBetterThan(a)) return 1;
+            return Long.compare(a.time, b.time);
+        }
+    }
+
+    public static class GameStats {
+        public final GameConfig gameConfig;
+        public int totalTicksPlayed = 0;
+        public int timesPlayed = 0;
+
+        public GameStats(GameConfig gameConfig) {
+            this.gameConfig = gameConfig;
+        }
+
+        public Map<String, GamePB> variantPBs = new TreeMap<>(
+                (a, b) -> {
+                    // Force "NONE" variant to come first, always
+                    a = a.replace(GameVariant.NONE.name(), " ");
+                    b = b.replace(GameVariant.NONE.name(), " ");
+
+                    return a.compareToIgnoreCase(b);
+                }
+        );
+
+        boolean hasPB() {
+            return !variantPBs.isEmpty();
+        }
+
+        String toPrintOutText(UUID playerUUID) {
+            var dataMap = new LinkedHashMap<String, Object>();
+            dataMap.put("Times played", this.timesPlayed);
+            dataMap.put("Total playtime", new Display.TicksTime(this.totalTicksPlayed, false));
+
+            for (var pbEntry : variantPBs.entrySet()) {
+                var valueText = pbEntry.getValue().toMsgPart();
+
+                boolean isNormalVariant = pbEntry.getKey().equals(GameVariant.NONE.name());
+                if (isNormalVariant) {
+                    // TODO: Implement leaderboard
+                    /*
+                    var lb = LEADERBOARD_MGR.getLeaderboard(gameConfig);
+                    if (lb != null) {
+                        int lbNumber = 0;
+                        for (int i = 0; i < lb.size(); i++) {
+                            if (lb.get(i).playerUUID().equals(playerUUID)) {
+                                lbNumber = i + 1;
+                                break;
+                            }
+                        }
+
+                        if (lbNumber > 0) {
+                            valueText = valueText.append(
+                                    TextPart.format(" &8[&f#&e" + lbNumber + "&8]")
+                            );
+                        }
+                    }
+                    */
+                }
+
+                dataMap.put("PB (" + pbEntry.getKey() + ")", valueText);
+            }
+
+            return mapToMsg(
+                    dataMap, "&a&l" + gameConfig.properName, 1
+            );
+        }
+
+        public static int compare(GamePB a, GamePB b) {
+            if (a.isBetterThan(b)) return -1;
+            if (b.isBetterThan(a)) return 1;
+            return 0;
+        }
+
+        public GamePB getNormalVariantPB() {
+            return variantPBs.get(GameVariant.NONE.name());
+        }
+    }
+
+    public final Map<GameConfig, GameStats> allGameStats = new TreeMap<>(
+            Comparator.comparing(a -> a.properName)
+    );
+
+    public PlayerStatsRecord(Player player) {
+        this.playerUUID = player.getUniqueId();
+        this.playerName = player.getName();
+    }
+
+    GameStats getGameStats(GameConfig gameConfig) {
+        return allGameStats.get(gameConfig);
+    }
+
+    public void syncPlayerName() {
+        var offlinePlayer = Bukkit.getOfflinePlayer(this.playerUUID);
+        if (offlinePlayer.hasPlayedBefore())
+            this.playerName = offlinePlayer.getName();
+    }
+
+    public void updateAfterPlaying(Gamer gamer, Game game, boolean finished) {
+        if (!gamer.player.getUniqueId().equals(this.playerUUID)) {
+            PLOG.warning(
+                    "PlayerStatsRecord: Player UUID mismatch" +
+                            " (" + gamer.player.getUniqueId() + " != " + this.playerUUID + ")"
+            );
+        }
+
+        if (gamer.isPlaying())
+            throw new RuntimeException("Player is still playing");
+
+        syncPlayerName();
+
+        allGameStats.putIfAbsent(game.config, new GameStats(game.config));
+        var gameStats = allGameStats.get(game.config);
+        gameStats.timesPlayed++;
+        gameStats.totalTicksPlayed += gamer.getTicksPlayedFor();
+
+        if (finished) {
+            var variant = game.variant;
+            var existingPB = gameStats.variantPBs.get(variant.name());
+            var candidatePB = new GamePB(game.config, gamer);
+            if (existingPB == null) {
+                gameStats.variantPBs.put(variant.name(), candidatePB);
+            } else {
+                if (candidatePB.isBetterThan(existingPB)) {
+                    gameStats.variantPBs.put(variant.name(), candidatePB);
+
+                    Display.sendMsg(gamer.player,
+                            "&aYou set a &LNEW PB& &ain {}&7: {}",
+                            "&6&l" + game.config.properName,
+                            "&b" + candidatePB.toMsgPart()
+                    );
+                }
+            }
+        }
+    }
+
+    public int calcTotalGamesPlayed() {
+        int sum = 0;
+        for (var val : allGameStats.values())
+            sum += val.timesPlayed;
+        return sum;
+    }
+
+    private static String mapToMsg(Map<String, Object> map, String title, int indentLevel) {
+        var pad = " ".repeat(indentLevel * 2);
+        StringBuilder sb = new StringBuilder(pad + title + "&7: ");
+
+        for (var entry : map.entrySet()) {
+            var k = entry.getKey();
+            var v = entry.getValue();
+            sb.append(
+                    Display.format("\n &7- &f{}&7: {}", k, v)
+            );
+        }
+
+        if (map.isEmpty())
+            sb.append("\n&7  (none)");
+
+        return sb.toString();
+    }
+
+    public String toPrintOutMsg() {
+        var offlinePlayer = Bukkit.getOfflinePlayer(playerUUID);
+        var firstPlayedMilli = offlinePlayer.getFirstPlayed();
+        var firstPlayedSecs = firstPlayedMilli / 1000;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(Display.format("&7-- &5&lStats for {} &7--", "&e&l" + playerName));
+        sb.append("\n").append(mapToMsg(
+                new HashMap<>(Map.of(
+                        "Total joins", numJoins,
+                        "First join date", new Display.UnixTime(firstPlayedSecs),
+                        "Total games played", calcTotalGamesPlayed()
+                )),
+                "&dBase stats", 0
+        ));
+
+
+        for (var gameEntry : allGameStats.entrySet())
+            sb.append("\n").append(gameEntry.getValue().toPrintOutText(playerUUID));
+
+        return Display.format(
+                "{}\n{}\n{}",
+                Display.Special.BAR,
+                sb.toString(),
+                Display.Special.BAR
+        );
+    }
+}
