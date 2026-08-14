04828f8d8ff1c43b5bb671b222da84fe5bd13722
ZealanL <36944229+ZealanL@users.noreply.github.com>
2026-08-13 23:25:05 -0700

    Fix ItemUtil.getSpawnEgg()

--- src/main/java/zealan/pgp/util/ItemUtil.java
+++ src/main/java/zealan/pgp/util/ItemUtil.java
@@ -3,14 +3,9 @@ package zealan.pgp.util;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.material.SpawnEgg;
 
 public class ItemUtil {
     public static ItemStack getSpawnEgg(EntityType entityType) {
-        ItemStack eggStack = new ItemStack(Material.MONSTER_EGG, 1);
-        SpawnEgg eggData = new SpawnEgg();
-        eggData.setSpawnedType(EntityType.CHICKEN);
-        eggStack.setData(eggData);
-        return eggStack;
+        return new ItemStack(Material.MONSTER_EGG, 1, entityType.getTypeId());
     }
 }
