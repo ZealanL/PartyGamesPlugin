cf34ea76704457bb93b1b578a3b8632457fe54cb
ZealanL <36944229+ZealanL@users.noreply.github.com>
2026-08-22 16:18:34 -0700

    Manually set flying speed when cleaning players

--- src/main/java/zealan/pgp/util/PlayerUtil.java
+++ src/main/java/zealan/pgp/util/PlayerUtil.java
@@ -33,6 +33,7 @@ public class PlayerUtil {
         player.setWalkSpeed(0.2f);
         player.setAllowFlight(player.getGameMode() == GameMode.CREATIVE);
         player.setFlying(false);
+        player.setFlySpeed(0.05f);
 
         // Clear XP
         player.setLevel(0);
